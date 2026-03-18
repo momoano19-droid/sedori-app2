ef09b31b89004fbed6a2ca71766e62d4ac0591c8
momoano19-droid <momo.ano19@gmail.com>
2026-03-19 02:53:00 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-const CACHE_NAME = "sedori-v4 ";
+const CACHE_NAME = "sedori-v8";
 
-const ASSETS = [
+const urlsToCache = [
   "./",
   "./index.html",
   "./report.html",
@@ -11,12 +11,14 @@ const ASSETS = [
   "./js/analytics.js",
   "./js/map.js",
   "./js/app.js",
-  "./js/report.js"
+  "./js/report.js",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
   self.skipWaiting();
 });
@@ -26,7 +28,7 @@ self.addEventListener("activate", event => {
     caches.keys().then(keys =>
       Promise.all(
         keys.map(key => {
-          if (key !== CACHE_NAME) {
+          if(key !== CACHE_NAME){
             return caches.delete(key);
           }
         })
@@ -37,7 +39,28 @@ self.addEventListener("activate", event => {
 });
 
 self.addEventListener("fetch", event => {
+  if(event.request.method !== "GET") return;
+
   event.respondWith(
-    caches.match(event.request).then(cached => cached || fetch(event.request))
+    caches.match(event.request).then(cached => {
+      if(cached) return cached;
+
+      return fetch(event.request)
+        .then(response => {
+          if(!response || response.status !== 200 || response.type !== "basic"){
+            return response;
+          }
+
+          const responseToCache = response.clone();
+          caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, responseToCache);
+          });
+
+          return response;
+        })
+        .catch(() => {
+          return caches.match("./index.html");
+        });
+    })
   );
 });
