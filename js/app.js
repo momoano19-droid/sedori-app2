9e92ee140ba3161139445080dc55f60bc0ae7b62
momoano19-droid <momo.ano19@gmail.com>
2026-03-21 01:12:16 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-const STORE_KEY = "sedori_stores_v3";
-const LOG_KEY = "sedori_logs_v3";
-const LAYOUT_KEY = "sedori_layout_v3";
-const AUTO_BACKUP_KEY = "sedori_auto_backup_v3";
+const STORE_KEY = "sedori_stores";
+const LOG_KEY = "sedori_logs";
+const LAYOUT_KEY = "sedori_layout";
+const AUTO_BACKUP_KEY = "sedori_auto_backup";
 const QUICK_LIMIT = 12;
 
 let stores = loadStores();
